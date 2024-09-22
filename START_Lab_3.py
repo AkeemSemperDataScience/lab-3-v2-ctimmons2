a1be25ad54721ee83556367344609e043b1ac5c4
Colin Timmons <ctimmons2@nait.ca>
2024-09-22 01:52:17 -0600

    Lab3 first commit

--- START_Lab_3.py
+++ START_Lab_3.py
@@ -4,13 +4,33 @@ def lab3Question1(number, cutoff):
     # Return True if the number is less than the cutoff, False otherwise
     # Also, print a statement of "[Number] is less than [cutoff]" or "[Number] is not less than [cutoff]"
     # Where the [Number] and [cutoff] are the actual numbers passed in
-    pass
+
+    #number comparison
+    if number < cutoff:
+        print(str(number) + ' is less than ' + str(cutoff))
+        return True
+    else:
+        print(str(number) + ' is not less than ' + str(cutoff))
+        return False
+
+#print(lab3Question1(10, 100))
+#print(lab3Question1(100, 100))
+#print(lab3Question1(100, 10))
 
 def lab3Question2(decimal_number):
     # Take in an argument of a float (decimal) number.
     # Return "zero" if the number is 0, "positive" if the number is positive, and "negative" if the number is negative
     # Return "invalid" if the input is not a float
-    pass
+    if decimal_number == 0:
+        return 'zero'
+    if decimal_number > 0:
+        return 'positive'
+    if decimal_number < 0:
+        return 'negative'
+    
+#print(lab3Question2(0))
+#print(lab3Question2(10))
+#print(lab3Question2(-10))
 
 def lab3Question3(year):
     # Take in a number that represents a year
@@ -19,13 +39,52 @@ def lab3Question3(year):
     # "19th century" if the year is between 1801 and 1900, 
     # "ancient" if the year is older
     # "invalid" if the input is not an acceptable year number. 
-    pass
+    
+    try:
+        year = int(year) 
+    except ValueError:
+        return "invalid"
+       
+    if year > 2001 and year < 2100:
+        return "21st century"
+    
+    if year > 1901 and year < 2000:
+        return "20th century"
+    
+    if year > 1801 and year < 1900:
+        return "19th century"
+    
+    if year < 1800:
+        return "ancient"
+    
+#print(lab3Question3('neer'))
+#print(lab3Question3(10))
+#print(lab3Question3(2050))
+#print(lab3Question3(1950))
+#print(lab3Question3(1850))
+
+
 
 def lab3Question4(number_1, number_2, number_3):
     # Take in three numbers as arguments
     # Return the largest of the three numbers
     # Return None if the inputs are not 3 numbers
-    pass
+
+    #check for numeric number input
+    try:
+        number_1 = int(number_1)
+        number_2 = int(number_2) 
+        number_3 = int(number_3) 
+    except ValueError:
+        return None
+    
+    
+    largest = max(number_1, number_2, number_3)
+    return (largest)
+
+#print(lab3Question4(2050, 20161, 201010))
+#print(lab3Question4(2050, 20161, 'beer'))
+
 
 def lab3Question5(temperature, scale_used):
     # Take in a temperature and the scale that the temperature is in - either "C" for Celsius or "F" for Fahrenheit (capitalized)
@@ -33,5 +92,34 @@ def lab3Question5(temperature, scale_used):
     # Return "Solid" if water is in solid state at that temperature
     # Return "Gas" if water is in gas state at that temperature
     # Return "Invalid" if the temperature or scale are invalid
-    pass
+    
+    try:
+        scale_used != 'C' or 'D'
+        temperature == int(temperature)
+    except ValueError:
+        return 'Invalid'
+    
+    if scale_used == 'F':
+        temperature = fahrenheit_to_celsius(temperature)
+        
+
+    if temperature >= 0 and temperature <= 100:
+        return 'Liquid'
+    if temperature < 0:
+        return 'Solid'
+    if temperature > 100:
+        return 'Gas'
+
+
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return round(celsius, 3)
+
+
+#print(lab3Question5(2050, 'C'))
+#print(lab3Question5(86, 'F'))
+#print(lab3Question5(-22, 'C'))
+#print(lab3Question5('beer', 'E'))
 
